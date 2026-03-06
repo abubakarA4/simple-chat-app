469ee286ea4d1a7be8f5ffde5a17e23e354e9829
Abubakar <bakarabbu026@email.com>
2026-03-06 10:35:30 +0530

    first chat app

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+http.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
